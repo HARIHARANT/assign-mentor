0a2e8a01357f2b75ae57a8e7625b930780796734
HARIHARANT <hariharancseksr@gmail.com>
2022-05-08 22:13:35 +0530

    second deploy

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const dotenv = require("dotenv");
 const mongo = require("./shared");
 const studentRouter = require("./router/student");
 const mentorRouter = require("./router/mentor");
+var cors = require('cors')
 dotenv.config();
 const app = express();
 app.use(express.json());
 mongo.connect();
-
+app.use(cors());
 app.use("/",(req,res,next)=>{    
     next();
    //res.send("Welcome to hallss");
